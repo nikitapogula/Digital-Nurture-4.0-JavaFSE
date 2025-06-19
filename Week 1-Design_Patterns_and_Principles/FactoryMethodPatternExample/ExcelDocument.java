af8d5cb7e113d1742eb5bc5c139af9c651fdd86f
nikitapogula <139140808+nikitapogula@users.noreply.github.com>
2025-06-19 23:29:46 +0530

    Delete Week 1-Design_Patterns_and_Principles directory

--- Week 1-Design_Patterns_and_Principles/FactoryMethodPatternExample/ExcelDocument.java
+++
@@ -1,9 +0,0 @@
-public class ExcelDocument implements Document {
-    public void open() {
-        System.out.println("Opening Excel document.");
-    }
-
-    public void close() {
-        System.out.println("Closing Excel document.");
-    }
-}
--- Week 1-Design_Patterns_and_Principles/FactoryMethodPatternExample/PdfDocument.java
+++
@@ -1,9 +0,0 @@
-public class PdfDocument implements Document {
-    public void open() {
-        System.out.println("Opening PDF Document.");
-    }
-
-    public void close() {
-        System.out.println("Closing PDF Document.");
-    }
-}
