7638aa08978b6f91dbab19540d7a9916b38c90a6
nikitapogula <139140808+nikitapogula@users.noreply.github.com>
2025-06-22 14:25:57 +0530

    Add files via upload

---
+++ Week 1-Design_Patterns_and_Principles/11-DependencyInjectionExample/Code/CustomerRepositoryImpl.java
@@ -0,0 +1,7 @@
+public class CustomerRepositoryImpl implements CustomerRepository {
+
+    public Customer findCustomerById(int id) {
+
+        return new Customer(id, "John Doe");
+    }
+}
---
+++ Week 1-Design_Patterns_and_Principles/11-DependencyInjectionExample/Code/CustomerService.java
@@ -0,0 +1,12 @@
+public class CustomerService {
+    private final CustomerRepository customerRepository;
+
+    // Constructor Injection
+    public CustomerService(CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
+
+    public Customer getCustomer(int id) {
+        return customerRepository.findCustomerById(id);
+    }
+}
